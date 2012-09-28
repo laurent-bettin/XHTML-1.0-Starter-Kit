7ccc391afcf6613cd3de123eb447776c7b1428f0
laurent-bettin <b.rhaze@gmail.com>
2012-09-28 15:11:33 +0200

    Revert "Update link to jQuery"
    This reverts commit 7bbe802ecbafbf418e13d741caeef0cf1ac648a9.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+﻿var front = {};
+
+(function (context) {
+    "use strict";
+
+    var privateVar;
+
+    var ui = {
+        init : function () {
+            $('html').removeAttr('class');
+            if (!privateVar) {
+                privateVar = 0;
+            }
+        }
+    };
+
+    context.ui = ui;
+
+})(front);
+
+$(document).ready(front.ui.init());
+//$(window).load(function(){ front.ui.init() });
